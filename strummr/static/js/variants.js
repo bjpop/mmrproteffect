3f02356ec0ca5998e2b5590e2da2b7469bed3645
Bernie Pope <bjpope@unimelb.edu.au>
2019-03-04 19:42:30 +1100

    Generalise pairwise plots of variant attributes

--- strummr/static/js/variants.js
+++ strummr/static/js/variants.js
@@ -1,35 +1,33 @@
 var global = {
-   'variant_information': [],    // all the rows of variant information
+    'variant_information': [], // all the rows of variant information
 };
 
 function show_variants_table() {
 
     var variants_table = $('#variants_table').DataTable({
         "dom": 'Blfrtip',
-        buttons: [
-            {
-               extend: "colvis",
-               className: "btn-sm btn-outline-primary",
-               titleAttr: 'Column visibility',
-               text: 'Columns'
+        buttons: [{
+                extend: "colvis",
+                className: "btn-sm btn-outline-primary",
+                titleAttr: 'Column visibility',
+                text: 'Columns'
             },
             {
-               extend: "copy",
-               className: "btn-sm btn-outline-primary",
-               titleAttr: 'Copy data',
-               text: 'Copy'
+                extend: "copy",
+                className: "btn-sm btn-outline-primary",
+                titleAttr: 'Copy data',
+                text: 'Copy'
             }
         ],
         "data": global['variant_information'],
         "order": [
-            [0, "asc"],  // sort by gene name first
-            [2, "asc"],  // then sort by genome position second
+            [0, "asc"], // sort by gene name first
+            [2, "asc"], // then sort by genome position second
         ],
         "scrollX": true,
-	"autoWidth": false,
+        "autoWidth": false,
         "pageLength": 25,
-        "columns": [
-            {
+        "columns": [{
                 data: "gene",
             },
             {
@@ -245,8 +243,7 @@ function show_variants_table() {
                 data: null
             },
         ],
-        "columnDefs": [
-	   {
+        "columnDefs": [{
             "targets": -1,
             "data": null,
             "render": function(data, type, row) {
@@ -254,80 +251,44 @@ function show_variants_table() {
                 var this_url = "variant/" + variant_identifiers.join("-");
                 return "<a href=" + this_url + ">details</a>";
             }
-          }
-	]
+        }]
     });
 
     // The below code is needed to get the DataTable to re-draw itself to get the
     // column widths correct. See https://github.com/DataTables/Responsive/issues/40
     // and http://stackoverflow.com/questions/8278981/datatables-on-the-fly-resizing/39157482#39157482
-    $($.fn.dataTable.tables( true ) ).css('width', '100%');
-    $($.fn.dataTable.tables( true ) ).DataTable().columns.adjust().draw();
+    $($.fn.dataTable.tables(true)).css('width', '100%');
+    $($.fn.dataTable.tables(true)).DataTable().columns.adjust().draw();
 
     return variants_table;
 }
 
+function main() {
 
-function plot_population_frequencies() {
-
-    frequecies_per_class = {};
-
-    var variants = [];
-    for (var i = 0; i < global['variant_information'].length; i++) {
-        var v = global['variant_information'][i];
-        var this_af = v.gnomad_af;
-        if (this_af) {
-            this_af = parseFloat(this_af);
-            var this_class = v.insight_class;
-            if (!(this_class in frequecies_per_class)) {
-                frequecies_per_class[this_class] = [];
-            }
-            frequecies_per_class[this_class].push(this_af);
-        }
-    }
-
-    var insight_classes = ['N/A', '1', '2', '3', '4', '5'];
-    var plot_traces = [];
-    var insight_classes = ['N/A', '1', '2', '3', '4', '5'];
-    var plot_traces = [];
-
-    insight_classes.forEach(function (class_name) {
-        if (class_name in frequecies_per_class) {
-            var this_class_frequencies = frequecies_per_class[class_name];
-        }
-        else {
-            var this_class_frequencies = [];
-        }
-        var this_trace = {
-            y: this_class_frequencies,
-            type: 'box',
-            name: 'Class ' + class_name,
-            boxpoints: 'all',
-            jitter: 0.3,
-            pointpos: -1.8,
-        };
-        plot_traces.push(this_trace);
+    /* event handlers */
+    $("#plot_x_axis").change(function() {
+        plot_variant_attributes(null);
     });
 
-    var layout = {
-        title: { text:'gnomAD population frequency versus InSiGHT class' },
-        yaxis: {
-            type: 'log',
-            autorange: true,
-            title: 'gnomAD population frequency (log scale)',
-        },
-        xaxis: {
-            title: 'InSiGHT class',
-        },
-        height: 600
-    };
+    /* event handlers */
+    $("#plot_y_axis").change(function() {
+        plot_variant_attributes(null);
+    });
 
-    Plotly.newPlot('population_frequencies', plot_traces, layout);
+    /* event handlers */
+    $("#log_x_axis").change(function() {
+        plot_variant_attributes(null);
+    });
 
-}
+    /* event handlers */
+    $("#log_y_axis").change(function() {
+        plot_variant_attributes(null);
+    });
 
-function main () {
-  $.ajax({
+    /* set up the axis options for plotting the variant attributes */
+    set_plot_select_options();
+
+    $.ajax({
         type: "GET",
         url: "/variants_data",
         cache: false,
@@ -335,7 +296,7 @@ function main () {
             global['variant_information'] = response.data;
             table = show_variants_table();
             global['variants_table'] = table;
-            plot_population_frequencies();
+            plot_variant_attributes(null);
         }
     });
 }
